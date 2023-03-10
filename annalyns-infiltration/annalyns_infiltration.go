5f875d6180865bc8e48d27c02cfa316b7d586e5e
Tim Vick <t.vick@yahoo.com>
2023-03-10 09:56:58 -0800

    chore: add annalyns-infiltration (#3)
    * chore: add annalyns-infiltration
    
    * style: update formatting

---
+++ annalyns-infiltration/annalyns_infiltration.go
@@ -0,0 +1,25 @@
+package annalyn
+
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+}
+
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
+}
+
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	return prisonerIsAwake && !archerIsAwake
+}
+
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	if archerIsAwake {
+		return false
+	}
+
+	if petDogIsPresent {
+		return true
+	}
+
+	return prisonerIsAwake && !knightIsAwake
+}
