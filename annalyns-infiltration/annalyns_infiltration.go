a1fed72cbde9a3b7216119407901fb3c14595ff1
Tim Vick <t.vick@yahoo.com>
2023-03-10 09:56:07 -0800

    style: update formatting

--- annalyns-infiltration/annalyns_infiltration.go
+++ annalyns-infiltration/annalyns_infiltration.go
@@ -1,25 +1,25 @@
 package annalyn
 
 func CanFastAttack(knightIsAwake bool) bool {
-  return !knightIsAwake
+	return !knightIsAwake
 }
 
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-  return knightIsAwake || archerIsAwake || prisonerIsAwake
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
 }
 
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-  return prisonerIsAwake && !archerIsAwake
+	return prisonerIsAwake && !archerIsAwake
 }
 
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-  if (archerIsAwake) {
-    return false
-  }
+	if archerIsAwake {
+		return false
+	}
 
-  if (petDogIsPresent) {
-    return true
-  }
+	if petDogIsPresent {
+		return true
+	}
 
-  return prisonerIsAwake && !knightIsAwake
+	return prisonerIsAwake && !knightIsAwake
 }
