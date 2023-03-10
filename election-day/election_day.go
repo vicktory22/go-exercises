0e25d823d06335a514800ee360e08b115ec75192
Tim Vick <t.vick@yahoo.com>
2023-03-10 10:24:22 -0800

    chore: add election day

---
+++ election-day/election_day.go
@@ -0,0 +1,50 @@
+package electionday
+
+import "fmt"
+
+func NewVoteCounter(initialVotes int) *int {
+	var votes *int
+
+	votes = &initialVotes
+
+	return votes
+}
+
+func VoteCount(counter *int) int {
+	if counter == nil {
+		return 0
+	}
+
+	return *counter
+}
+
+func IncrementVoteCount(counter *int, increment int) {
+	*counter = *counter + increment
+}
+
+func NewElectionResult(candidateName string, votes int) *ElectionResult {
+	var result *ElectionResult
+
+	result = &ElectionResult{
+		Name:  candidateName,
+		Votes: votes,
+	}
+
+	return result
+}
+
+func DisplayResult(result *ElectionResult) string {
+	return fmt.Sprintf("%s (%d)", result.Name, result.Votes)
+}
+
+func DecrementVotesOfCandidate(results map[string]int, candidate string) {
+	_, exists := results[candidate]
+
+	if !exists {
+		return
+	}
+
+	results[candidate] -= 1
+
+	return
+}
