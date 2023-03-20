d14509ecdd09cdb7be6e6a23ea10ae974b1660f5
Tim Vick <t.vick@yahoo.com>
2023-03-20 13:33:17 -0700

    chore: formatting

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -3,21 +3,21 @@ package collatzconjecture
 import "fmt"
 
 func collate(n, counter int) int {
-    if n == 1 {
-        return counter
-    }
+	if n == 1 {
+		return counter
+	}
 
-    if n % 2 == 0 {
-        return collate(n / 2, counter + 1)
-    }
+	if n%2 == 0 {
+		return collate(n/2, counter+1)
+	}
 
-    return collate(3 * n + 1, counter + 1)
+	return collate(3*n+1, counter+1)
 }
 
 func CollatzConjecture(n int) (int, error) {
-    if n < 1 {
-        return 0, fmt.Errorf("invalid input")
-    }
+	if n < 1 {
+		return 0, fmt.Errorf("invalid input")
+	}
 
-    return collate(n, 0), nil
+	return collate(n, 0), nil
 }
